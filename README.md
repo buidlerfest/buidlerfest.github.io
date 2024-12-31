c2540a1cd15e7deeea30387f3a78f38a13beaac8
Jingles <jinglescode@gmail.com>
2024-12-31 22:33:09 +0800

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # buidlerfest.github.io
+
+This is the repository for the [Cardano BuidlerFest website](https://buidlerfest.github.io/).
---
+++ components/header.js
@@ -0,0 +1,20 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <nav>
+          <a href="/index.html">Home</a>
+          <a href="/agenda.html">Agenda</a>
+          <a href="/venue.html">Venue</a>
+          <a href="/register.html">Register</a>
+        </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define("header-component", Header);
